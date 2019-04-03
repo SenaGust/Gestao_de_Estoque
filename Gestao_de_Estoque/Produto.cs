60d6b8d372cffaebca422bbbc0385bec04aeec02
João Soares <1078157@sga.pucminas.br>
2019-04-03 17:10:53 -0300

    Classe Produto

--- Gestao_de_Estoque/Produto.cs
+++ Gestao_de_Estoque/Produto.cs
@@ -8,6 +8,22 @@ namespace Gestao_de_Estoque
 {
     class Produto
     {
+        public string ID { get; private set; }
+        public string NomeProduto { get; private set; }
+        public int Categoria { get; private set; }        
 
+        public Produto()
+        {
+            ID = "0-0000";
+            NomeProduto = "AAAAAA";
+            Categoria = 0;
+        }
+
+        public Produto(string id, string nomeProduto, int categoria)
+        {
+            this.ID = id;
+            this.NomeProduto = nomeProduto;
+            this.Categoria = categoria;
+        }
     }
 }
