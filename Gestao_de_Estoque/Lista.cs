9f579b87a6fa7d412e59a1407ccaa9e37f182332
Gustavo Sena <gustavosena98@LIVE.COM>
2019-04-03 20:30:11 -0300

    Implementação do método ToString()

--- Gestao_de_Estoque/Lista.cs
+++ Gestao_de_Estoque/Lista.cs
@@ -8,6 +8,26 @@ namespace Gestao_de_Estoque
 {
     class Lista
     {
+        public Elemento Primeiro { get; private set; }
+        public Elemento Ultimo { get; private set; }
 
+        public override string ToString()
+        {
+            StringBuilder auxImpressao = new StringBuilder();
+            Elemento atual = Primeiro;
+
+            while (atual.Prox != null)
+            {
+                auxImpressao.AppendLine(atual.Prox.MeuDado.ToString());
+                atual = atual.Prox;
+            }
+
+            return auxImpressao.ToString();
+        }
+
+        public void UnirListas(Lista lista)
+        {
+
+        }
     }
 }
