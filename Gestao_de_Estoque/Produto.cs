77e4e52ca6352dd5e002f5908db3ab28bb2285d5
Gustavo Sena <gustavosena98@LIVE.COM>
2019-04-05 11:02:56 -0300

    Consertando um erro meu

--- Gestao_de_Estoque/Produto.cs
+++ Gestao_de_Estoque/Produto.cs
@@ -12,9 +12,9 @@ namespace Gestao_de_Estoque
         public string ID { get; private set; }
         public string NomeProduto { get; private set; }
         public int Categoria { get; private set; }
-        public double PrecoCusto { get; private set; }
-        public double MargemLucro { get; private set; }
-        public double Imposto { get; private set; }
+        public double PrecoCusto { get; set; }
+        public double MargemLucro { get; set; }
+        public double Imposto { get; set; }
         #endregion
 
         #region Construtor
