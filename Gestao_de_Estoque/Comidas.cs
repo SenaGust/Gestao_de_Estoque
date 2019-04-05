bb35c7482f3721538dbfcbaf6387569f670d750e
João Soares <1078157@sga.pucminas.br>
2019-04-05 12:33:02 -0300

    Modelo provisório do arquivo: tipo; id; nome; precoCusto; lucroMin; LucroMax Modelo provisório do construtor: id; nome; precoCusto; lucroMin; LucroMax

--- Gestao_de_Estoque/Comidas.cs
+++ Gestao_de_Estoque/Comidas.cs
@@ -9,7 +9,7 @@ namespace Gestao_de_Estoque
     class Comidas : Produto
     {
         #region Construtor
-        public Comidas(string id, string nome, double preco, double mLucro) : base(id, nome, preco, mLucro)
+        public Comidas(string id, string nome, double preco, double mLucroMin, double mLucroMax) : base(id, nome, preco, mLucroMin, mLucroMax)
         {
             Imposto = 0.1;
         }
@@ -18,12 +18,12 @@ namespace Gestao_de_Estoque
         #region Métodos Abstratos
         public override double CalcImposto()
         {
-            return (PrecoCusto + (PrecoCusto * MargemLucro)) * Imposto;
+            return (PrecoCusto + (PrecoCusto * MargemLucroMIN)) * Imposto;
         }
 
         public override double CalcPrecoVenda()
         {
-            return PrecoCusto + (PrecoCusto * MargemLucro) + CalcImposto();
+            return PrecoCusto + (PrecoCusto * MargemLucroMIN) + CalcImposto();
         }
         #endregion
 
@@ -31,7 +31,7 @@ namespace Gestao_de_Estoque
         public override string ToString()
         {
             return String.Format("Id: {0}; Categoria: Comidas; Nome do produto: {1}; Custo: {2}; Margem de lucro: {3}",
-                ID, NomeProduto, PrecoCusto, MargemLucro);
+                ID, NomeProduto, PrecoCusto, MargemLucroMIN);
         }
         public bool Equals(Produto product)
         {
--- Gestao_de_Estoque/Produto.cs
+++ Gestao_de_Estoque/Produto.cs
@@ -12,17 +12,19 @@ namespace Gestao_de_Estoque
         public string ID { get; private set; }
         public string NomeProduto { get; private set; }        
         public double PrecoCusto { get; set; }
-        public double MargemLucro { get; set; }
+        public double MargemLucroMIN { get; set; }
+        public double MargemLucroMAX { get; set; }
         public double Imposto { get; set; }
         #endregion
 
         #region Construtor
-        public Produto(string ID, string NomeProduto, double PrecoCusto, double MargemLucro)
+        public Produto(string ID, string NomeProduto, double PrecoCusto, double MargemLucroMIN, double MargemLucroMAX)
         {
             this.ID = ID;
             this.NomeProduto = NomeProduto;            
             this.PrecoCusto = PrecoCusto;
-            this.MargemLucro = MargemLucro;            
+            this.MargemLucroMIN = MargemLucroMIN;
+            this.MargemLucroMAX = MargemLucroMAX;
         }
         #endregion
 
